db8d6e24436019eaea5e0d1263af36b2ddc4df7c
Greg Sarandi <sarandi@usc.edu>
2023-02-11 01:34:22 -0800

    Updated get_sent() function

--- mail_server.py
+++ mail_server.py
@@ -24,11 +24,34 @@ def load_mail() -> List[Dict[str, str]]:
 
 def save_mail(mail: List[Dict[str, str]]) -> None:
     """TODO: fill out this docstring (using the load_mail docstring as a guide)
-    """
+    
+    Parameters: 
+        The save_mail method takes in list of dictionaries called mail
+
+    Functionality:
+        It seems like the code takes in the list of dictionaries and uses joinpath method to
+        to create a file path with the present directory and the write_text is used to write 
+        the list of dictionaries to a json file which json.dump() helps with by converting the 
+        python objects in the list into json objects.
+
+    Returns: 
+        the function returns none which means nothing is returned
+    """ 
     thisdir.joinpath('mail_db.json').write_text(json.dumps(mail, indent=4))
 
 def add_mail(mail_entry: Dict[str, str]) -> str:
     """TODO: fill out this docstring (using the load_mail docstring as a guide)
+     Parameters: 
+        The add_mail method takes in a dictionary called mail_entry
+
+    Functionality:
+       It loads in the mail that is already on the json file and then adds the parameter,
+       mail_entry by using append. Then it assigns a unique id for the mail entry and then 
+       saves the list of mail back to the json file.
+
+    Returns: 
+        the function returns a string back which is a unique id for ther mail entry
+
     """
     mail = load_mail()
     mail.append(mail_entry)
@@ -38,6 +61,17 @@ def add_mail(mail_entry: Dict[str, str]) -> str:
 
 def delete_mail(mail_id: str) -> bool:
     """TODO: fill out this docstring (using the load_mail docstring as a guide)
+    Parameters: 
+        The delete_mail function takes in a string called mail_id which is the unique id that
+        each mail gets
+
+    Functionality:
+        The function takes in the mail id and checks if the pail id exists by going throigh 
+        the entire list of the id. If the id is found, it deletes the mail of the list, and then
+        saves the list back into the json file
+    
+    Returns: 
+        returns true if mail was successfully deleted or false if it wasn't
     """
     mail = load_mail()
     for i, entry in enumerate(mail):
@@ -50,6 +84,18 @@ def delete_mail(mail_id: str) -> bool:
 
 def get_mail(mail_id: str) -> Optional[Dict[str, str]]:
     """TODO: fill out this docstring (using the load_mail docstring as a guide)
+    Parameters: 
+        The get_mail function takes in a string called mail_id which is the unique id that
+        each mail gets
+
+    Functionality:
+        The function takes in the mail id and checks if the pail id exists by going throigh 
+        the entire list of the id. If the id is found, it prints the mail that the id that 
+        it corresponds with
+    
+    Returns: 
+        The function returns an optional meaning that though it says it returns a dictionary,
+        an optional allows None to be returned too
     """
     mail = load_mail()
     for entry in mail:
@@ -60,6 +106,17 @@ def get_mail(mail_id: str) -> Optional[Dict[str, str]]:
 
 def get_inbox(recipient: str) -> List[Dict[str, str]]:
     """TODO: fill out this docstring (using the load_mail docstring as a guide)
+    Parameters: 
+        The get_inbox function takes in string called recipient 
+
+    Functionality:
+        The function loads in the mail from the json file and initalizes a list called inbox. Afterwards, it
+        checks if the list from the json has the recipient that was given in the parameters and appends it to 
+        the inbox list. Afterwards, it returns all the emails that have that specific recipient associated 
+        to it
+    
+    Returns: 
+        the function return a list of dictionarys which contain emails associated to the recipient
     """
     mail = load_mail()
     inbox = []
@@ -71,6 +128,17 @@ def get_inbox(recipient: str) -> List[Dict[str, str]]:
 
 def get_sent(sender: str) -> List[Dict[str, str]]:
     """TODO: fill out this docstring (using the load_mail docstring as a guide)
+    Parameters: 
+        The get_sent function takes in string called sender
+
+    Functionality:
+        The function loads in the mail from the json file and initalizes a list called sent. Afterwards, it
+        checks if the list from the json has the specific sender that was given in the parameters and appends 
+        it to the inbox list. Afterwards, it returns all the emails that have that specific sender associated 
+        to it
+    
+    Returns: 
+        the function return a list of dictionarys which contain emails associated to the sender
     """
     mail = load_mail()
     sent = []
@@ -146,7 +214,16 @@ def get_inbox_route(recipient: str):
 #   @app.route('/mail/sent/<sender>', ...)
 @app.route('/mail/sent/<sender>', methods=['GET'])
 def get_sent_route(sender: str):
-    res = jsonify(get_inbox(sender))
+    """
+    Summary: Gets all mail entries for a sender from the json file
+
+    Args:
+        sender (str): The sender of the mail
+
+    Returns:
+        list: A list of dictionaries representing the mail entries
+    """
+    res = jsonify(get_sent(sender))
     res.status_code = 200
     return res
 
